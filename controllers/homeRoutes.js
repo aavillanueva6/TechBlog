71bb5c5d0c59db7d603666ba73e3389c7d9264d6
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-24 00:10:08 -0700

    adds initial templating and get route for dashboard page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,6 +17,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/dashboard', async (req, res) => {
+  // need to add login verification
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll();
+
+    // Serialize data so the template can read it
+    const posts = postData.map((element) => element.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('dashboard', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {

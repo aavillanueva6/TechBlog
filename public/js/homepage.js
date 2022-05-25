5c08b90e47f87b2d053ae1d6a561629e32cf0a03
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-24 17:06:03 -0700

    adds logic and needed handlers to link to a single post

---
+++ public/js/homepage.js
@@ -0,0 +1,12 @@
+const cardContainer = document.querySelector('.customCardContainer');
+
+cardContainer.addEventListener('click', (event) => {
+  const target = event.target;
+  const dataIdDiv = target.closest('[data-id]');
+  const id = dataIdDiv.getAttribute('data-id');
+
+  console.log(target);
+  console.log(event);
+  console.log(id);
+  window.location.href = `/singlepost/${id}`;
+});

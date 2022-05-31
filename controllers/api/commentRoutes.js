b807dbb8b1374932808d309d756bc4d25ff06155
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-30 23:10:29 -0700

    adds post route for comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,6 +3,12 @@ const { Comment } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
+    const user_id = req.session.user_id;
+    req.body.user_id = user_id;
+
+    const commentData = await Comment.create(req.body);
+
+    res.status(200).json(commentData);
   } catch (err) {
     res.status(400).json(err);
   }

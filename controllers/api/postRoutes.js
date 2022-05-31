ac446f0da5692053a42c2741b07f9ba7191bd22c
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-30 22:12:07 -0700

    adds user_id to create a post route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,6 +4,11 @@ const { Post } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     // create a new post from body of request
+    console.log(req.body);
+    const user_id = req.session.user_id;
+    console.log(user_id);
+    req.body.user_id = user_id;
+    console.log(req.body);
     const postData = await Post.create(req.body);
 
     res.status(200).json({ post: postData, message: 'post created!' });
